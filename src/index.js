772a442bb329de7bda89f3c3ac9e7a65087e89ee
Jonathan Momperousse <bklynbest718@gmail.com>
2018-11-11 08:21:21 +0000

    connection b/w firestore and redux established

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, 
+  compose(
+
+  applyMiddleware(thunk.withExtraArgument({getFirestore, getFirebase})),
+  reduxFirestore(fbConfig),
+  reactReduxFirebase(fbConfig)
+  )
+);
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
