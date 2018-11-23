4bd577b5931923e9d122f75fd0b6a2aa2fda971f
Jonathan Momperousse <bklynbest718@gmail.com>
2018-11-23 06:59:09 +0000

    added firebase cloud functionality to frontend

--- src/components/project/ProjectList.js
+++ src/components/project/ProjectList.js
@@ -5,14 +5,13 @@ import { Link } from 'react-router-dom';
 const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
-      {projects && projects.map(project => {
+      { projects && projects.map(project => {
         return(
           <Link to={'/project/' + project.id }>
             <ProjectSummary project={project} key={project.id} />
           </Link>
         );
       })}
-      
     </div>
   );
 };
--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import moment from 'moment'
 
+
 const ProjectSummary = ({ project }) => {
     return (
-        <div className="project-list section">
+    <div className="project-list section">
      <div className="card z-depth-0 project-summary">
        <div className="card-content grey-text text-darken-3">
-         <span className="card-title">{project.title}</span>
+         <span className="card-title">{project.title} </span>
          <p>Posted by { project.authorFirstName } { project.authorLastName }</p>
          <p className="grey-text">{moment(project.createdAt.toDate()).format('MMMM Do YYYY, h:mm:ss a')}</p>
        </div>
+        <div className='row'>
+          <div className='col s6 '>
+          <i class="material-icons ">delete</i>
+          </div>
+        </div>
      </div>
     </div>
     );
